39a833fe6a4a49e0bc90b8bc99d3912f9ea1d364
OneSpongeMan <rozhokmax2003@gmail.com>
2025-08-07 16:21:34 +0400

    created initial migration, updated starup settings

--- Quiz.API/Program.cs
+++ Quiz.API/Program.cs
@@ -1,13 +1,20 @@
+using Quiz.DAL.EF;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 namespace Quiz.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args); 
+            var startup = new StartUp(builder);
+            startup.ConfigureServices();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
